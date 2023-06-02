a37795ebb0da05e4c293241072a43fe8923df1e9
RQuicho <raymondquicho@gmail.com>
2023-06-01 17:36:35 -0700

    adding routes that update and delete existing cupcakes

--- app.py
+++ app.py
@@ -38,5 +38,25 @@ def create_cupcake():
         )
     db.session.add(new_cupcake)
     db.session.commit()
-
     return (jsonify(cupcake=new_cupcake.serialize()), 201)
+
+@app.route('/api/cupcakes/<int:id>', methods=['PATCH'])
+def update_cupcake(id):
+    """Update existing cupcake"""
+
+    cupcake = Cupcake.query.get_or_404(id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes/<int:id>', methods=['DELETE'])
+def delete_cupcake(id):
+    """Delete existing cupcake"""
+
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message="cupcake deleted")
